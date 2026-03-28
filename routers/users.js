cc56d7083f91484b8b17dd75ae1450df29ab2403
X. Perez <xavierperez890@gmail.com>
2026-03-27 21:10:30 -0400

    Added comments

--- routers/users.js
+++ routers/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.route('/').get((req, res)=>{
     res.render('users/list', {users: users});
-}).post((req, res)=>{
+}).post((req, res)=>{                    //data we want to get from the form
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const gender = req.body.gender;
@@ -11,7 +11,7 @@ router.route('/').get((req, res)=>{
     const isValid = firstName !== "";
     if(isValid){
         console.log(`Adding user: ${firstName}`);
-        users.push({firstName: firstName, lastName: lastName, gender: gender, age: age});
+        users.push({firstName: firstName, lastName: lastName, gender: gender, age: age}); //adds new user to the array
         res.redirect('/users');
     }
     else{
@@ -21,7 +21,7 @@ router.route('/').get((req, res)=>{
 });
 
 router.get('/new', (req, res)=>{
-    res.render('users/new', {firstName: "", lastName: "", gender: "", age: ""});
+    res.render('users/new', {firstName: "", lastName: "", gender: "", age: ""}); //gets name and additional information from the user and sends it to the server to be added to the users array
 });
 
 router.route('/:id').get((req, res)=>{
@@ -34,7 +34,7 @@ router.route('/:id').get((req, res)=>{
     res.send(`Updating User Data: ${req.params.id}`);
 });
 
-const users = [{firstName:"Xavier", lastName:"Perez", gender:"Male", age:20}, {firstName:"Goomaral", lastName:"Bayaraa", gender:"Female", age:21}];
+const users = [{firstName:"Xavier", lastName:"Perez", gender:"Male", age:20}, {firstName:"Goomaral", lastName:"Bayaraa", gender:"Female", age:21}];  //creates users and thier information
 
 router.param('id', (req, res, next, id)=>{
     req.user = users[id];
